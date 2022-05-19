3fb2f0f24f58ea6b37b51c2a886bad1c420c2545
Miriael <ravafiel@protonmail.com>
2022-05-19 02:44:25 +0200

    Refactor the entire playRound function

--- js.js
+++ js.js
@@ -1,46 +1,36 @@
 //Create a function that randomly selects the computer's move and stores that in a variable
-let computerMove = ''
-let playerMove = ''
-let wins = 0
-let playCount = 0
 const MOVES = ['rock', 'paper', 'scissors']
 function computerPlays() {
   computerMove = MOVES[Math.floor(Math.random() * MOVES.length)]
   return computerMove;
 }
 //Create a function that plays a single round and returns who won
-function playRound() {
-  playerMove = prompt('Rock, paper or scissors?')
-  computer = computerPlays()
-  if (playerMove == undefined) {
-    playerMove = ''
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == undefined) {
+    playerSelection = ''
   }
-  if (playerMove.toLowerCase() != 'rock' && playerMove.toLowerCase() != 'scissors' && playerMove.toLowerCase() != 'paper') {
+  if (playerSelection.toLowerCase() != 'rock' && playerSelection.toLowerCase() != 'scissors' && playerSelection.toLowerCase() != 'paper') {
     alert('Invalid input.');
-  } else if (playerMove.toLowerCase() == 'rock' && computer == 'paper') {
-    alert('You lost!');
-    playCount += 1;
-  } else if (playerMove.toLowerCase() == 'rock' && computer == 'scissors') {
-    alert('You won!');
-    playCount += 1;
-    wins += 1;
-  } else if (playerMove.toLowerCase() == 'paper' && computer == 'rock') {
-    alert('You won!');
-    playCount += 1;
-    wins += 1;
-  } else if (playerMove.toLowerCase() == 'paper' && computer == 'scissors') {
-    alert('You lost!')
-    playCount += 1
-  } else if (playerMove.toLowerCase() == 'scissors' && computer == 'rock') {
-    alert('You lost!');
-    playCount += 1;
-  } else if (playerMove.toLowerCase() == 'scissors' && computer == 'paper') {
-    alert('You won!');
-    playCount += 1
-    wins += 1
-  } else if (playerMove.toLowerCase() == computer) {
-    alert("It's a tie!")
-    playCount += 1
+  } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
+    return "You lose! Paper beats rock."
+  } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+    return "You win! Rock beats scissors."
+  } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+    return "You win! Paper beats rock."
+  } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+    return "You lose! Scissors beat paper."
+  } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+    return "You lose! Rock beats scissors."
+  } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+    return "You win! Scissors beat paper."
+  } else if (playerSelection.toLowerCase() == computerSelection) {
+    return "It's a tie!"
   }
 } 
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound(prompt('Rock, paper or scissors?'), computerPlays())
+  }
+}
 //Create a function that calls the play function X times and keeps track of games and score
