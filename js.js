265b719c04e9a01bd0c8729a67bc5deb5a55eff4
Miriael <ravafiel@protonmail.com>
2022-05-25 23:01:54 +0200

    Refactor the logic to support a GUI

--- js.js
+++ js.js
@@ -1,3 +1,41 @@
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const results = document.querySelector('#gameLog');
+const wins = document.querySelector('#wins');
+const losses = document.querySelector('#losses');
+let gameCount = 0;
+let winCount = 0;
+let lossCount = 0;
+wins.textContent = `Wins: ${winCount}`;
+losses.textContent = `Losses: ${lossCount}`;
+results.setAttribute('style', 'white-space: pre;');
+
+
+rockBtn.addEventListener('click', () => {
+  playRound('rock', computerPlays());
+});
+
+paperBtn.addEventListener('click', () => {
+  playRound('paper', computerPlays());
+});
+
+scissorsBtn.addEventListener('click', () => {
+  playRound('scissors', computerPlays());
+});
+
+function victory() {
+  gameCount += 1;
+  winCount += 1;
+  wins.textContent = `Wins: ${winCount}`;
+}
+
+function loss() {
+  gameCount += 1;
+  lossCount += 1;
+  losses.textContent = `Losses: ${lossCount}`;
+}
+
 function computerPlays() {
   let moves = ['rock', 'paper', 'scissors']
   computerMove = moves[Math.floor(Math.random() * moves.length)];
@@ -5,49 +43,26 @@ function computerPlays() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == undefined) {
-    return null;
-  }
-  if (playerSelection.toLowerCase() != 'rock' && playerSelection.toLowerCase() != 'scissors' && playerSelection.toLowerCase() != 'paper') {
-    alert('Invalid input.');
-    return null;
-  } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
-    alert("You lose! Paper beats rock.");
-    return false;
+  if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
+    results.textContent += "You lose! Paper beats rock.\n";
+    loss();
   } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
-    alert("You win! Rock beats scissors.");
-    return true;
+    results.textContent += "You win! Rock beats scissors.\n";
+    victory();
   } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
-    alert("You win! Paper beats rock.");
-    return true;
+    results.textContent += "You win! Paper beats rock.\n";
+    victory();
   } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
-    alert("You lose! Scissors beat paper.");
-    return false;
+    results.textContent += "You lose! Scissors beat paper.\n";
+    loss();
   } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
-    alert("You lose! Rock beats scissors.");
-    return false;
+    results.textContent += "You lose! Rock beats scissors.\n";
+    loss();
   } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
-    alert("You win! Scissors beat paper.");
-    return true;
+    results.textContent += "You win! Scissors beat paper.\n";
+    victory();
   } else if (playerSelection.toLowerCase() == computerSelection) {
-    alert("It's a tie!")
-    return null;
+    results.textContent += "It's a tie!\n";
+    gameCount += 1;
   }
 } 
-
-function game() {
-  let wins = 0;
-  let losses = 0;
-  for (let i = 0; i < 5; i++) {
-    result = playRound(prompt('Rock, paper or scissors?'), computerPlays())
-    if (result == null) {
-      continue;
-    } else if (result == true) {
-      wins += 1;
-    }
-    else if (result == false) {
-      losses += 1;
-    }
-  }
-  alert(`Wins: ${wins} Losses: ${losses}`);
-  }
